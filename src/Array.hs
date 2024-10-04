6aa7fc03066e25bec806cf0ae94129474075148a
qqwy <qqwy@gmx.com>
2024-10-04 22:47:09 +0200

    The strict pair of two arrays can itself be considered an array,
    without any performance loss!

--- src/Array.hs
+++ src/Array.hs
@@ -127,6 +127,47 @@ instance (Prim a) => Array (PrimArray a) where
     PrimArray.copyPrimArray dst idx src (idx + 1) (size src - idx - 1)
     pure dst
 
+instance (Array a, Array b) => Array (a, b) where
+    type Item (a, b) = (Item a, Item b)
+    {-# INLINE empty #-}
+    empty = (empty, empty)
+    {-# INLINE size #-}
+    size (a, _) = size a
+    {-# INLINE get #-}
+    get (!a, !b) idx =
+        let (# x #) = get a idx
+            (# y #) = get b idx
+        in (# (x, y) #)
+    {-# INLINE set #-}
+    set (!a, !b) idx (x, y) = (set a idx x, set b idx y)
+    {-# INLINE insert #-}
+    insert (!a, !b) idx (x, y) = (insert a idx x, insert b idx y)
+    {-# INLINE snoc #-}
+    snoc (!a, !b) (x, y) = (snoc a x, snoc b y)
+    {-# INLINE delete #-}
+    delete (!a, !b) idx = (delete a idx, delete b idx)
+
+data StrictPair a b = StrictPair !a !b
+
+instance (Array a, Array b) => Array (StrictPair a b) where
+    type Item (StrictPair a b) = StrictPair (Item a) (Item b)
+    {-# INLINE empty #-}
+    empty = StrictPair empty empty
+    {-# INLINE size #-}
+    size (StrictPair a _) = size a
+    {-# INLINE get #-}
+    get (StrictPair a b) idx =
+        let (# x #) = get a idx
+            (# y #) = get b idx
+        in (# (StrictPair x y) #)
+    {-# INLINE set #-}
+    set (StrictPair a b) idx (StrictPair x y) = StrictPair (set a idx x) (set b idx y)
+    {-# INLINE insert #-}
+    insert (StrictPair a b) idx (StrictPair x y) = StrictPair (insert a idx x) (insert b idx y)
+    {-# INLINE snoc #-}
+    snoc (StrictPair a b) (StrictPair x y) = StrictPair (snoc a x) (snoc b y)
+    {-# INLINE delete #-}
+    delete (StrictPair a b) idx = StrictPair (delete a idx) (delete b idx)
 
 
 foldl' :: Array ary => (b -> Item ary -> b) -> b -> ary -> b
@@ -139,18 +180,21 @@ foldl' f = \ z0 ary0 -> go ary0 (size ary0) 0 z0
             (# x #) -> go ary n (i+1) (f z x)
 {-# INLINE foldl' #-}
 
-foldlP' :: (Array aryA, Array aryB) => (b -> Item aryA -> Item aryB -> b) -> b -> aryA -> aryB -> b
-foldlP' f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0) 0 z0
-  where
-    go aryA aryB n i !z
-        | i >= n = z
-        | otherwise
-        = case (# get aryA i, get aryB i #) of
-            (# (# x #), (# y #) #) -> go aryA aryB n (i+1) (f z x y)
-{-# INLINE foldlP' #-}
+-- foldlP' :: (Array aryA, Array aryB) => (b -> Item aryA -> Item aryB -> b) -> b -> aryA -> aryB -> b
+-- foldlP' f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0) 0 z0
+--   where
+--     go aryA aryB n i !z
+--         | i >= n = z
+--         | otherwise
+--         = case (# get aryA i, get aryB i #) of
+--             (# (# x #), (# y #) #) -> go aryA aryB n (i+1) (f z x y)
+-- {-# INLINE foldlP' #-}
 
 sum :: SmallArray Int -> SmallArray Int -> Int
-sum = Array.foldrP' (const (+)) 0
+sum !a !b = (Array.foldr' (\(StrictPair k v) acc -> k + v + acc) 0) $ (StrictPair a b)
+
+-- sum2 :: SmallArray Int -> SmallArray Int -> Int
+-- sum2 = Array.foldrP' (\k v acc -> k + v + acc) 0
 
 foldr' :: Array ary => (Item ary -> b -> b) -> b -> ary -> b
 foldr' f = \ z0 ary0 -> go ary0 (size ary0 - 1) z0
@@ -161,15 +205,15 @@ foldr' f = \ z0 ary0 -> go ary0 (size ary0 - 1) z0
       = go ary (i - 1) (f x z)
 {-# INLINE foldr' #-}
 
-foldrP' :: (Array aryA, Array aryB) => (Item aryA -> Item aryB -> b -> b) -> b -> aryA -> aryB -> b
-foldrP' f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0 - 1) z0
-  where
-    go !_aryA !_aryB (-1) z = z
-    go !aryA !aryB i !z
-      | (# x #) <- get aryA i
-      , (# y #) <- get aryB i
-      = go aryA aryB (i - 1) (f x y z)
-{-# INLINE foldrP' #-}
+-- foldrP' :: (Array aryA, Array aryB) => (Item aryA -> Item aryB -> b -> b) -> b -> aryA -> aryB -> b
+-- foldrP' f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0 - 1) z0
+--   where
+--     go !_aryA !_aryB (-1) z = z
+--     go !aryA !aryB i !z
+--       | (# x #) <- get aryA i
+--       , (# y #) <- get aryB i
+--       = go aryA aryB (i - 1) (f x y z)
+-- {-# INLINE foldrP' #-}
 
 foldr :: Array ary => (Item ary -> b -> b) -> b -> ary -> b
 foldr f = \ z0 ary0 -> go ary0 (size ary0) 0 z0
@@ -181,15 +225,15 @@ foldr f = \ z0 ary0 -> go ary0 (size ary0) 0 z0
             (# x #) -> f x (go ary n (i+1) z)
 {-# INLINE foldr #-}
 
-foldrP :: (Array aryA, Array aryB) => (Item aryA -> Item aryB -> b -> b) -> b -> aryA -> aryB -> b
-foldrP f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0) 0 z0
-  where
-    go aryA aryB n i z
-        | i >= n = z
-        | otherwise
-        = case (# get aryA i, get aryB i #) of
-            (# (# x #), (# y #) #) -> f x y (go aryA aryB n (i+1) z)
-{-# INLINE foldrP #-}
+-- foldrP :: (Array aryA, Array aryB) => (Item aryA -> Item aryB -> b -> b) -> b -> aryA -> aryB -> b
+-- foldrP f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0) 0 z0
+--   where
+--     go aryA aryB n i z
+--         | i >= n = z
+--         | otherwise
+--         = case (# get aryA i, get aryB i #) of
+--             (# (# x #), (# y #) #) -> f x y (go aryA aryB n (i+1) z)
+-- {-# INLINE foldrP #-}
 
 foldl :: Array ary => (b -> Item ary -> b) -> b -> ary -> b
 foldl f = \ z0 ary0 -> go ary0 (size ary0 - 1) z0
@@ -201,12 +245,12 @@ foldl f = \ z0 ary0 -> go ary0 (size ary0 - 1) z0
 {-# INLINE foldl #-}
 
 
-foldlP :: (Array aryA, Array aryB) => (b -> Item aryA -> Item aryB -> b) -> b -> aryA -> aryB -> b
-foldlP f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0 - 1) z0
-  where
-    go _aryA _aryB (-1) z = z
-    go aryA aryB i z
-      | (# x #) <- get aryA i
-      , (# y #) <- get aryB i
-      = f (go aryA aryB (i - 1) z) x y
-{-# INLINE foldlP #-}
+-- foldlP :: (Array aryA, Array aryB) => (b -> Item aryA -> Item aryB -> b) -> b -> aryA -> aryB -> b
+-- foldlP f = \ z0 aryA0 aryB0 -> go aryA0 aryB0 (size aryB0 - 1) z0
+--   where
+--     go _aryA _aryB (-1) z = z
+--     go aryA aryB i z
+--       | (# x #) <- get aryA i
+--       , (# y #) <- get aryB i
+--       = f (go aryA aryB (i - 1) z) x y
+-- {-# INLINE foldlP #-}
