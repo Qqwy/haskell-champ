07e38e16df41cffe8b2774b8ab024015bfc6bc13
Marten Wijnja <marten.wijnja@channable.com>
2025-03-05 23:33:21 +0100

    Expose the Unlifted HashMap types from Champ.HashMap and Champ

--- src/Champ.hs
+++ src/Champ.hs
@@ -4,9 +4,15 @@ module Champ (
     Champ.HashMap.HashMapBL,
     Champ.HashMap.HashMapBB,
     Champ.HashMap.HashMapBU,
+    Champ.HashMap.HashMapBUl,
     Champ.HashMap.HashMapUL,
     Champ.HashMap.HashMapUB,
     Champ.HashMap.HashMapUU,
+    Champ.HashMap.HashMapUUl,
+    Champ.HashMap.HashMapUlL,
+    Champ.HashMap.HashMapUlB,
+    Champ.HashMap.HashMapUlU,
+    Champ.HashMap.HashMapUlUl,
     -- ** HashSet
     Champ.HashSet.HashSetB,
     Champ.HashSet.HashSetU,
@@ -18,3 +24,4 @@ module Champ (
 
 import Champ.HashMap qualified
 import Champ.HashSet qualified
+import Champ.Internal.Storage qualified
