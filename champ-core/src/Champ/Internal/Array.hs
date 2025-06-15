4e6ccf3e79d05963d92da164406b12d303e09a19
Marten Wijnja <marten.wijnja@channable.com>
2025-06-16 00:01:00 +0200

    Fix compile warnings on older GHC versions

--- champ-core/src/Champ/Internal/Array.hs
+++ champ-core/src/Champ/Internal/Array.hs
@@ -63,7 +63,8 @@ import Data.Primitive.Unlifted.Class qualified as Unlifted
 import Data.Primitive.Unlifted.SmallArray (SmallMutableUnliftedArray_ (..), SmallUnliftedArray_ (..), mapSmallUnliftedArray, unsafeThawSmallUnliftedArray, shrinkSmallMutableUnliftedArray)
 import Data.Primitive.Unlifted.SmallArray.Primops (SmallMutableUnliftedArray# (SmallMutableUnliftedArray#), SmallUnliftedArray# (SmallUnliftedArray#))
 import GHC.Exts (SmallArray#, SmallMutableArray#)
-import Prelude hiding (foldl, foldl', foldr, length, null, read)
+
+import Prelude hiding (foldl, foldr, length, null, read)
 
 -- | Helper newtype to implement `PrimUnlifted` for any datatype,
 -- part of making `StrictSmallArray`/`StrictSmallMutableArray` work for any `a`.
